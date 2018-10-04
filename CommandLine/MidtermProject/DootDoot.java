329b2de526300534b9418065e261ee1e8b8bdcd1
Safyire <safyire.n8@gmail.com>
2018-10-04 08:23:48 -0700

    Updated DootDoot

--- CommandLine/MidtermProject/DootDoot.java
+++ CommandLine/MidtermProject/DootDoot.java
@@ -4,6 +4,7 @@ import edu.cmu.ri.createlab.hummingbird.HummingbirdRobot;
 
 public class DootDoot extends HummingbirdRobot {
     public static void main(String[] args) {
-        System.out.println("doot doot...this is ");
+        System.out.println("doot doot...this is the Skeleton Jukebox!");
+        System.out.println("Pick a song to start!");
     }
 }
