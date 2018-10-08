8fbcfe148b91103759695a27cdec2887fc66ebc9
Safyire <safyire.n8@gmail.com>
2018-10-08 08:56:22 -0700

    Added printed song list in DootDoot

--- CommandLine/MidtermProject/DootDoot.java
+++ CommandLine/MidtermProject/DootDoot.java
@@ -14,15 +14,23 @@ public class DootDoot extends HummingbirdRobot {
 
         System.out.println("doot doot...this is the Skeleton Jukebox!");
         System.out.println("Pick a song to start!");
-        System.out.println("1 - Test audio clip");
+        System.out.println("1: Test audio clip\n" +
+                "2: Careless Whisper Romantic Instrumental\n" +
+                "3: Darude - Sandstorm for Brass Quintet\n" +
+                "4: Megalovania Undertale Trombone Quartet\n" +
+                "5: Mii Channel Theme Sax Quartet\n" +
+                "6: Queen - Don't Stop Me Now Trombone Quartet\n" +
+                "7: Trumpet - I Want It That Way - Backstreet Boys\n" +
+                "8: Vitas - The 7th Element for Brass Quintet");
         int songNumberInput = s.nextInt();
 
         //TODO: Wake me up inside
         //TODO: Special Megalovania lights
         if(songNumberInput < 1) {
-            System.out.println("That's not the right output!");
+            System.out.print("That's not the right output!");
             //TODO: Play a spooky audio clip here
         } else {
+            System.out.print("Now playing: " + SongList.getSong(songNumberInput));
             doot.playClip(PlaySound.sound(SongList.getSong(songNumberInput)));
             //I assume each byte in the array takes a ms to process...so it'll Thread.sleep for that amount of bytes
             Thread.sleep(PlaySound.sound(SongList.getSong(songNumberInput)).length);
